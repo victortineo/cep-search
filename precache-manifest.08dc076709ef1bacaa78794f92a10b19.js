0336db6ebe75950b2f4cfaae1175d38903a71786
Victor Tineo <victor.tineo141@gmail.com>
2020-01-26 23:13:26 -0300

    Updates

---
+++ precache-manifest.08dc076709ef1bacaa78794f92a10b19.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2ad28fa2de3491c310670b0e624873e",
+    "url": "/cep-search/index.html"
+  },
+  {
+    "revision": "067780dc07b5ea2faadd",
+    "url": "/cep-search/static/js/2.aa40680e.chunk.js"
+  },
+  {
+    "revision": "dfeea506dedb1f8ad64e8d3829765f8f",
+    "url": "/cep-search/static/js/2.aa40680e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "59afcc7ec4ace54e7e87",
+    "url": "/cep-search/static/js/main.506d3a7e.chunk.js"
+  },
+  {
+    "revision": "ac11fb9d510fbae30746",
+    "url": "/cep-search/static/js/runtime-main.a061db22.js"
+  }
+]);
